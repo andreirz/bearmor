1a73fc7a858b8e693b1819bae2fb21e6997c6fea
Andrei <andrei@bearz.ee>
2025-10-05 22:58:12 +0300

    Bump version to 0.1.5

--- bearmor-security.php
+++ bearmor-security.php
@@ -3,7 +3,7 @@
  * Plugin Name: Bearmor Security
  * Plugin URI: https://bearmor.com
  * Description: Lightweight, robust WordPress security plugin for SMBs.
- * Version: 0.1.4
+ * Version: 0.1.5
  * Author: Bearmor Security Team
  * Author URI: https://bearmor.com
  * License: GPL v2 or later
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define plugin constants
-define( 'BEARMOR_VERSION', '0.1.4' );
+define( 'BEARMOR_VERSION', '0.1.5' );
 define( 'BEARMOR_PLUGIN_FILE', __FILE__ );
 define( 'BEARMOR_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'BEARMOR_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
